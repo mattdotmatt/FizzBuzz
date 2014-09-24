fd5881ddb5656c22de1f114280422dac22475c8b
mattdotmatt <mpyoung@gmail.com>
2014-09-24 13:08:43 +0100

    Initial commit
    Fizz Buzz in F# as learning exercise

---
+++ FizzBuzz.Tests/Tests.fs
@@ -0,0 +1,28 @@
+﻿module FizzBuzz.Tests.``A game of bowling``
+
+open NUnit.Framework
+open FsUnit
+
+[<Test>] 
+let ``1 should return 1`` () =
+    FB.fizzBuzz 1 |> should equal "1"
+
+[<Test>] 
+let ``2 should return 2`` () =
+    FB.fizzBuzz 2 |> should equal "2"
+
+[<Test>] 
+let ``3 should return fizz`` () =
+    FB.fizzBuzz 3 |> should equal "fizz"
+
+[<Test>] 
+let ``5 should return buzz`` () =
+    FB.fizzBuzz 5 |> should equal "buzz"
+
+[<Test>] 
+let ``6 should return fizz`` () =
+    FB.fizzBuzz 6 |> should equal "fizz"
+
+[<Test>] 
+let ``15 should return fizz buzz`` () =
+    FB.fizzBuzz 15 |> should equal "fizz buzz"
---
+++ FizzBuzz/FB.fs
@@ -0,0 +1,11 @@
+﻿module FB
+
+    let fizzBuzz (x) =
+        let fizzable (y) = y % 3 = 0
+        let buzzable (y) = y % 5 = 0
+
+        match x with
+        | _ when (fizzable x && buzzable x) -> "fizz buzz"
+        | _ when (fizzable x) -> "fizz"
+        | _ when (buzzable x) -> "buzz"
+        | _ -> x.ToString()
